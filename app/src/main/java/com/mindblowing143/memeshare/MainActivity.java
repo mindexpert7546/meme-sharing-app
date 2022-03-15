f10d27fcdd0848f832376a731c4e10721d321b13
mindexpert7546 <kk7615117@gmail.com>
2022-03-15 20:58:26 +0530

    first commit

---
+++ app/src/main/java/com/mindblowing143/memeshare/MainActivity.java
@@ -0,0 +1,110 @@
+package com.mindblowing143.memeshare;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView memeView;
+    Button nextButton, shareButton;
+    ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        memeView = findViewById(R.id.memeView);
+        shareButton = findViewById(R.id.shareButton);
+        nextButton = findViewById(R.id.nextButton);
+        progressBar = findViewById(R.id.prograssBarr);
+
+
+    }
+
+    public  void loadMeme(){
+        String url = "https://meme-api.herokuapp.com/gimme";
+        progressBar.setVisibility(progressBar.VISIBLE);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String url = response.getString("url");
+                           // Glide.with(MainActivity.this).load(url).into(memeView);
+                            Glide.with(MainActivity.this).load(url).listener(new RequestListener<Drawable>() {
+                                @Override
+                                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                    progressBar.setVisibility(progressBar.INVISIBLE);
+                                    return  false;
+
+                                }
+
+                                @Override
+                                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                    progressBar.setVisibility(progressBar.INVISIBLE);
+                                    return  false;
+                                }
+                            }).into(memeView);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toast.makeText(MainActivity.this,"Something is wrong ! ",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+    }
+
+    public void shareMeme(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "wow cool meme !");
+
+        startActivity(intent);
+
+    }
+
+    public void nextMeme(View view) {
+        loadMeme();
+    }
+}
